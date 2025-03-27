136a6d59e8866142520c7fb691205b7e689bad97
emily <Emily_Damazio05@hotmail.com>
2025-03-27 00:43:52 -0300

    Exercicios da Tv

--- Java-Anatomia/src/Emily/Java/Segundasemana/SegundoDia.java
+++ Java-Anatomia/src/Emily/Java/Segundasemana/SegundoDia.java
@@ -3,8 +3,13 @@ package Emily.Java.Segundasemana;
 public class SegundoDia {
 
     public static void main(String[] args) {
-        
-        
+
+        double soma = 10 + 8.5;
+
+
+
+
+
     }
     
 }
